ff2c7d228d17cc70a908e14ea19c26e5c222db9b
Olga Savon <olchiksavon@gmail.com>
2022-10-24 17:15:04 +0600

    added action creator matchable for categories

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,5 +1,6 @@
-import { Category, CATEGORY_ACTION_TYPES } from './category.types';
-import { CategoryAction } from './category.action';
+import { Category } from './category.types';
+import { fetchCategoryStart, fetchCategorySuccess, fetchCategoryFailed } from './category.action';
+import { AnyAction } from 'redux';
 
 export type CategoriesState = {
   readonly categories: Category[];
@@ -15,16 +16,19 @@ export const CATEGORIES_INITIAL_STATE: CategoriesState = {
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {} as CategoryAction
-) => {
-  switch (action.type) {
-    case CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return { ...state, isLoading: true };
-    case CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categories: action.payload, isLoading: false };
-    case CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: action.payload, isLoading: false };
-    default:
-      return state;
+  action = {} as AnyAction
+): CategoriesState => {
+  if (fetchCategoryStart.match(action)) {
+    return { ...state, isLoading: true };
   }
+
+  if (fetchCategorySuccess.match(action)) {
+    return { ...state, categories: action.payload, isLoading: false };
+  }
+
+  if (fetchCategoryFailed.match(action)) {
+    return { ...state, error: action.payload, isLoading: false };
+  }
+
+  return state;
 };
--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,3 +1,5 @@
+import { AnyAction } from 'redux';
+
 export type ActionWithPayload<T, P> = {
   type: T;
   payload: P;
@@ -7,10 +9,35 @@ export type Action<T> = {
   type: T;
 };
 
-// function overloading
+// type overloading for the function
 export function createAction<T extends string, P>(type: T, payload: P): ActionWithPayload<T, P>;
 export function createAction<T extends string>(type: T, payload: void): Action<T>;
 
 export function createAction<T extends string, P>(type: T, payload: P) {
   return { type, payload };
+}
+
+type Matchable<AC extends () => AnyAction> = AC & {
+  // AC - action creator
+  type: ReturnType<AC>['type']; // using Return type
+  match(action: AnyAction): action is ReturnType<AC>; // using type predicate function and type narrowing
+};
+
+// type overloading for the function using intersection types
+export function withMatcher<AC extends () => AnyAction & { type: string }>(
+  actionCreator: AC
+): Matchable<AC>;
+
+export function withMatcher<AC extends (...args: any[]) => AnyAction & { type: string }>(
+  actionCreator: AC
+): Matchable<AC>;
+
+export function withMatcher(actionCreator: Function) {
+  const type = actionCreator().type;
+  return Object.assign(actionCreator, {
+    type,
+    match(action: AnyAction) {
+      return action.type === type;
+    },
+  });
 }
